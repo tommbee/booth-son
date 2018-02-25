a31417e9157416ff89aaade60b5385207c53340d
Thomas Bennett <tombennett86@hotmail.co.uk>
2018-02-25 22:00:14 +0000

    User ORM set up

--- src/AppBundle/Entity/User/CmsUser.php
+++ src/AppBundle/Entity/User/CmsUser.php
@@ -2,9 +2,26 @@
 
 namespace AppBundle\Entity\User;
 
-class CmsUser {
-    
-    protected $name;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class CmsUser extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     public function getName()
     {
