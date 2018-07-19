fe204a0f8b336486cf898609fae66ebbc6e3898e
Thomas Bennett <tombennett86@hotmail.co.uk>
2018-07-19 22:05:58 +0100

    add all project routes

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,5 +20,24 @@ class ProjectController extends Controller
         return $this->render('default/projects.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/projects/{id}", requirements={"id" = "\d+"}, name="projectview")
+     * @param int $id
+     */
+    public function showAction($id)
+    {
+        echo 'show!';
+    }
+
+    /**
+     * @Route("/projects", name="projectsave")
+     * @Method({"POST"})
+     * @param Request $request
+     */
+    public function saveAction(Request $request)
+    {
+        echo 'save!';
     }
 }
