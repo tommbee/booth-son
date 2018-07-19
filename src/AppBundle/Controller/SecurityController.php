3537c0030971327a65e42eea2d9c618be3e85b0e
Thomas Bennett <tombennett86@hotmail.co.uk>
2018-07-19 22:06:20 +0100

    Add redirect routes after logging in and logging out

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use FOS\UserBundle\Controller\SecurityController as BaseController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class SecurityController extends BaseController
 {    
@@ -11,6 +12,10 @@ class SecurityController extends BaseController
      */
     protected function renderLogin(array $data)
     {
+        if (($this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))) {
+            return new RedirectResponse('/profile');
+        }
+
         return $this->render('Security/login.html.twig', $data);
     }
 }
