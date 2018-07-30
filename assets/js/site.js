907301cbe1fa16f809be3403c45947b0615057a5
Thomas Bennett <tombennett86@hotmail.co.uk>
2018-07-30 22:26:32 +0100

    styles for header and nav

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,50 @@
 const $ = require('jquery');
+let Wills = Wills || {};
+const siteURL = 'http://' + top.location.host.toString();
+const $burger = $(".burger a");
+const $mmenu = $(".mobile_menu");
 
 $(document).ready(function () {
     window.loadEverything = function () {
-        $(".loading").addClass("hide").delay(2000).queue(function () { $(this).remove(); });
+        $("body").addClass("loaded");
+        $(".loading").addClass("hide").delay(1000).queue(function () { $(this).addClass('back'); });
     }
-    //Wills.Main.init();
+    Wills.Main.init();
 });
+
+Wills.Main = (function () {
+
+    function init() {
+        console.log("Main::init()");
+        initListener();
+    }
+
+    function initListener() {
+        const menuLinks = $('a[href^="' + siteURL + '"]:not(".normal-trans"), a[href^="/"]:not(".normal-trans"), a[href^="./"]:not(".normal-trans"), a[href^="../"]:not(".normal-trans")');
+        menuLinks.on('click', function(e) {
+            if (e.metaKey || e.ctrlKey || e.shiftKey) return;
+            e.preventDefault();
+
+            const linkLocation = this.href;
+
+            function redirectPage() {
+                window.location = linkLocation;
+            }
+
+            $("body").addClass('is-exiting');
+
+            setTimeout(function() {
+                redirectPage();
+            }, 100);
+        });
+
+        $burger.on('click', function(e) {
+            $mmenu.toggleClass('open');
+        });
+    }
+
+    return {
+        init: init
+    };
+
+})();
