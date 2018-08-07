f750fac6ff73f40c218c8d3be55cafe7e254eff2
Thomas Bennett <tombennett86@hotmail.co.uk>
2018-08-07 22:08:37 +0100

    animations

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,6 @@
 import { Preloader } from './preloader'
 const $ = require('jquery');
+const tilt = require('tilt.js');
 
 export class Main {
 
@@ -15,6 +16,22 @@ export class Main {
         console.log("Main::init()");
         this.initListener();
         this.initPreloader();
+        this.initTilts();
+    }
+
+    initTilts() {
+        // $('.js-tilt').tilt({
+        //     maxTilt:        10,
+        //     perspective:    1500,   // Transform perspective, the lower the more extreme the tilt gets.
+        //     //easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        //     scale:          1.01,      // 2 = 200%, 1.5 = 150%, etc..
+        //     speed:          300,    // Speed of the enter/exit transition.
+        //     transition:     true,   // Set a transition on enter/exit.
+        //     disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+        //     reset:          true,   // If the tilt effect has to be reset on exit.
+        //     glare:          true,  // Enables glare effect
+        //     maxGlare:       0.5       // From 0 - 1.
+        // });
     }
 
     initPreloader() {
